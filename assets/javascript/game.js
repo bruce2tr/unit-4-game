6cd9c6a9b13bc51e579115edc4463956a22dd5f0
bruce2tr <bruce.trammell@gmail.com>
2019-05-18 07:02:34 -0400

    js file complete & working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,6 +35,9 @@ $(document).ready(function(){
         console.log("1Clicked");
         if(crystalFirst==null){
             crystalFirst = Math.floor(Math.random()*20 +1);
+            crystalSecond = Math.floor(Math.random()*20 +1);
+            crystalThird = Math.floor(Math.random()*20 +1);
+            crystalFourth = Math.floor(Math.random()*20 +1);
             userScore = userScore + crystalFirst;
             $("#totalScore").text(crystalFirst);
             // console.log(userScore);
@@ -42,7 +45,36 @@ $(document).ready(function(){
         }
         else{
             userScore = userScore + crystalFirst;
-            $("#totalScore").text(userScore);
+            console.log(crystalFirst);
+            if(userScore<randomNumber){
+                $("#totalScore").text(userScore);  
+            }
+            else if(userScore == randomNumber){
+                alert("You Win!");
+                randomNumber = Math.floor((Math.random()*71) +30);
+                $("#randomNumberDisplay").text(randomNumber);
+                wins = wins + 1;
+                $("#winsID").text("Wins: " + wins);
+                userScore = 0;
+                $("#totalScore").text(userScore);
+                crystalFirst = Math.floor(Math.random()*20 +1);
+                crystalSecond = Math.floor(Math.random()*20 +1);
+                crystalThird = Math.floor(Math.random()*20 +1);
+                crystalFourth = Math.floor(Math.random()*20 +1);
+            }
+            else{
+                alert("You Lose!");
+                randomNumber = Math.floor((Math.random()*71) +30);
+                $("#randomNumberDisplay").text(randomNumber);
+                losses = losses + 1;
+                $("#lossesID").text("Losses: " + losses);
+                userScore = 0;
+                $("#totalScore").text(userScore);
+                crystalFirst = Math.floor(Math.random()*20 +1);
+                crystalSecond = Math.floor(Math.random()*20 +1);
+                crystalThird = Math.floor(Math.random()*20 +1);
+                crystalFourth = Math.floor(Math.random()*20 +1);
+            };
             // console.log(userScore);
             // console.log(crystalFirst);
         };
@@ -50,7 +82,10 @@ $(document).ready(function(){
     $("#crystal_2").click(function(){
         console.log("2Clicked");
         if(crystalSecond==null){
+            crystalFirst = Math.floor(Math.random()*20 +1);
             crystalSecond = Math.floor(Math.random()*20 +1);
+            crystalThird = Math.floor(Math.random()*20 +1);
+            crystalFourth = Math.floor(Math.random()*20 +1);
             userScore = userScore + crystalSecond;
             $("#totalScore").text(crystalSecond);
             console.log(userScore);
@@ -58,15 +93,45 @@ $(document).ready(function(){
         }
         else{
             userScore = userScore + crystalSecond;
-            $("#totalScore").text(userScore);
-            console.log(userScore);
-            console.log(crystalSecond);
+            if(userScore<randomNumber){
+                $("#totalScore").text(userScore);  
+                console.log(crystalSecond);
+            }
+            else if(userScore == randomNumber){
+                alert("You Win!");
+                randomNumber = Math.floor((Math.random()*71) +30);
+                $("#randomNumberDisplay").text(randomNumber);
+                wins = wins + 1;
+                $("#winsID").text("Wins: " + wins);
+                userScore = 0;
+                $("#totalScore").text(userScore);
+                crystalFirst = Math.floor(Math.random()*20 +1);
+                crystalSecond = Math.floor(Math.random()*20 +1);
+                crystalThird = Math.floor(Math.random()*20 +1);
+                crystalFourth = Math.floor(Math.random()*20 +1);
+            }
+            else{
+                alert("You Lose!");
+                randomNumber = Math.floor((Math.random()*71) +30);
+                $("#randomNumberDisplay").text(randomNumber);
+                losses = losses + 1;
+                $("#lossesID").text("Losses: " + losses);
+                userScore = 0;
+                $("#totalScore").text(userScore);
+                crystalFirst = Math.floor(Math.random()*20 +1);
+                crystalSecond = Math.floor(Math.random()*20 +1);
+                crystalThird = Math.floor(Math.random()*20 +1);
+                crystalFourth = Math.floor(Math.random()*20 +1);
+            };
         };
     });
     $("#crystal_3").click(function(){
         console.log("3Clicked");
         if(crystalThird==null){
+            crystalFirst = Math.floor(Math.random()*20 +1);
+            crystalSecond = Math.floor(Math.random()*20 +1);
             crystalThird = Math.floor(Math.random()*20 +1);
+            crystalFourth = Math.floor(Math.random()*20 +1);
             userScore = userScore + crystalThird;
             $("#totalScore").text(crystalThird);
             console.log(userScore);
@@ -74,14 +139,44 @@ $(document).ready(function(){
         }
         else{
             userScore = userScore + crystalThird;
-            $("#totalScore").text(userScore);
-            console.log(userScore);
-            console.log(crystalThird);
+            if(userScore<randomNumber){
+                $("#totalScore").text(userScore); 
+                console.log(crystalThird); 
+            }
+            else if(userScore == randomNumber){
+                alert("You Win!");
+                randomNumber = Math.floor((Math.random()*71) +30);
+                $("#randomNumberDisplay").text(randomNumber);
+                wins = wins + 1;
+                $("#winsID").text("Wins: " + wins);
+                userScore = 0;
+                $("#totalScore").text(userScore);
+                crystalFirst = Math.floor(Math.random()*20 +1);
+                crystalSecond = Math.floor(Math.random()*20 +1);
+                crystalThird = Math.floor(Math.random()*20 +1);
+                crystalFourth = Math.floor(Math.random()*20 +1);
+            }
+            else{
+                alert("You Lose!");
+                randomNumber = Math.floor((Math.random()*71) +30);
+                $("#randomNumberDisplay").text(randomNumber);
+                losses = losses + 1;
+                $("#lossesID").text("Losses: " + losses);
+                userScore = 0;
+                $("#totalScore").text(userScore);
+                crystalFirst = Math.floor(Math.random()*20 +1);
+                crystalSecond = Math.floor(Math.random()*20 +1);
+                crystalThird = Math.floor(Math.random()*20 +1);
+                crystalFourth = Math.floor(Math.random()*20 +1);
+            };
         };
     });
     $("#crystal_4").click(function(){
         console.log("4Clicked");
         if(crystalFourth==null){
+            crystalFirst = Math.floor(Math.random()*20 +1);
+            crystalSecond = Math.floor(Math.random()*20 +1);
+            crystalThird = Math.floor(Math.random()*20 +1);
             crystalFourth = Math.floor(Math.random()*20 +1);
             userScore = userScore + crystalFourth;
             $("#totalScore").text(crystalFourth);
@@ -90,9 +185,36 @@ $(document).ready(function(){
         }
         else{
             userScore = userScore + crystalFourth;
-            $("#totalScore").text(userScore);
-            console.log(userScore);
-            console.log(crystalFourth);
+            if(userScore<randomNumber){
+                $("#totalScore").text(userScore);  
+                console.log(crystalFourth);
+            }
+            else if(userScore == randomNumber){
+                alert("You Win!");
+                randomNumber = Math.floor((Math.random()*71) +30);
+                $("#randomNumberDisplay").text(randomNumber);
+                wins = wins + 1;
+                $("#winsID").text("Wins: " + wins);
+                userScore = 0;
+                $("#totalScore").text(userScore);
+                crystalFirst = Math.floor(Math.random()*20 +1);
+                crystalSecond = Math.floor(Math.random()*20 +1);
+                crystalThird = Math.floor(Math.random()*20 +1);
+                crystalFourth = Math.floor(Math.random()*20 +1);
+            }
+            else{
+                alert("You Lose!");
+                randomNumber = Math.floor((Math.random()*71) +30);
+                $("#randomNumberDisplay").text(randomNumber);
+                losses = losses + 1;
+                $("#lossesID").text("Losses: " + losses);
+                userScore = 0;
+                $("#totalScore").text(userScore);
+                crystalFirst = Math.floor(Math.random()*20 +1);
+                crystalSecond = Math.floor(Math.random()*20 +1);
+                crystalThird = Math.floor(Math.random()*20 +1);
+                crystalFourth = Math.floor(Math.random()*20 +1);
+            };
         };
     });
 });
